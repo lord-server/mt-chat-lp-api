2b4267cca26f7d9dde07c273656c898198321b23
googolgl <googolgl@gmail.com>
2021-04-01 13:16:12 +0300

    fix

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 - Add : **secure.http_mods = lp_api**
 
 ### Add parameters if needed in to minetest.confg
-* **lp_api.url** - LongPoll server url (Default: **http://mtdbot.pp.ua:8003**)
+* **lp_api.url** - LongPoll server url (Default: **http://localhost:8003**)
 * **lp_api.channel_id** - Channel ID in Discord. Rightclick on the Discord text channel you want the bot to interact with and press "Copy ID", and then insert value in this (Default: **minetest**)
 * **lp_api.timeout** - Timeout connection (Default: **30**)
 * **lp_api.header** - Header if needed (Default: **nil**)
-* **lp_api.subscriber.timeout** - Timeout for subscribe (Default: **26**)
+* **lp_api.subscriber.timeout** - Timeout for subscribe (Default: **15**)
 * **lp_api.router.cmd** - Support commands or not (Default: **false**)
 
 License
--- init.lua
+++ init.lua
@@ -1,17 +1,24 @@
 lp_api = {}
 lp_api.http = minetest.request_http_api()
-lp_api.url = minetest.settings:get("lp_api.url") or "http://mtdbot.pp.ua:8003"
+lp_api.url = minetest.settings:get("lp_api.url") or "http://localhost:8003"
 lp_api.channel_id = minetest.settings:get("lp_api.channel_id") or "minetest"
 lp_api.timeout = minetest.settings:get("lp_api.timeout") or 30
 -- For secure connect set header value (Authorization: Basic bG9naW46cGFzc3dvcmQ=)
--- where bG9naW46cGFzc3dvcmQ= is the base64(login:password)
-lp_api.header = minetest.settings:get("lp_api.header") or nil
+-- where bG9naW46cGFzc3dvcmQ= is the base64(login:password) "Content-Type: application/json; charset=utf8"
+lp_api.header = minetest.settings:get("lp_api.header") or  nil
 
-function lp_api.request_http(ext_url,pd,response_handler)
-    local req = {url = lp_api.url..ext_url, post_data = pd, extra_headers = {lp_api.header}, timeout = lp_api.timeout}
+function lp_api.request_http(ext_url,pd,response_handler,pmethod)
+    local req = {
+        url = lp_api.url..ext_url,
+        method = pmethod,
+        post_data = pd, 
+        extra_headers = {lp_api.header}, 
+        timeout = lp_api.timeout
+    }
+
     lp_api.http.fetch(req, function(result)
         pcall(response_handler, result)
-    end)
+    end)  
 end
 
 if lp_api.http then
